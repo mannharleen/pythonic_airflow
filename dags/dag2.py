cda52e89a731c98ccf9b7e665d3f6a188af6021f
Mann <Harleen.Mann@originenergy.com.au>
2018-07-27 09:46:07 +1000

    added emr hook to start emr

--- dags/dag2.py
+++ dags/dag2.py
@@ -6,9 +6,9 @@ from airflow.models import Variable
 
 START = datetime.combine(datetime.today() - timedelta(days=2), datetime.min.time()) + timedelta(hours=10)
 DAG_NAME = 'emr_model_building'
-#ENV = 'DEV'
+# Use airflow env variables
 ENV = Variable.get("ENV", default_var="DEV")
-HIVEPASSWORD = Variable.get("HIVEPASSWORD", default_var="DEV")
+HIVEPASSWORD = Variable.get("HIVEPASSWORD", default_var="DEV")      # should ideally be dev.hivepassword !!!
 
 print("obtained ENV as {}".format(ENV))
 
@@ -39,10 +39,10 @@ launch_emr = """
 --service-role EMR_DefaultRole \
 --enable-debugging \
 --release-label emr-5.13.0 \
---log-uri 's3n://aws-logs/aws-logs-319593511656-ap-southeast-2/elasticmapreduce/' \
+--log-uri 's3n://aws-logs-319593511656-ap-southeast-2/elasticmapreduce/' \
 --name 'My cluster' \
 --instance-groups '[{"InstanceCount":2,"InstanceGroupType":"CORE","InstanceType":"m4.2xlarge","Name":"Core - 2"},{"InstanceCount":1,"InstanceGroupType":"MASTER","InstanceType":"m4.xlarge","Name":"Master - 1"}]' \
---configurations '[{"Classification":"hive-site","Properties":{"javax.jdo.option.ConnectionUserName":"root","javax.jdo.option.ConnectionDriverName":"org.mariadb.jdbc.Driver","javax.jdo.option.ConnectionPassword":"{{params.HIVEPASSWORD}}","javax.jdo.option.ConnectionURL":"jdbc:mysql://devmetastore1.clmis5yhnool.ap-southeast-2.rds.amazonaws.com:3306/hive?createDatabaseIfNotExist=true"},"Configurations":[]}, {"Classification": "capacity-scheduler","Properties": {"yarn.scheduler.capacity.resource-calculator":"org.apache.hadoop.yarn.util.resource.DominantResourceCalculator"}}, {"Classification": "yarn-site","Properties": { "yarn.resourcemanager.scheduler.class": "org.apache.hadoop.yarn.server.resourcemanager.scheduler.fair.FairScheduler"} }]' \
+--configurations '[{"Classification":"hive-site","Properties":{"javax.jdo.option.ConnectionUserName":"root","javax.jdo.option.ConnectionDriverName":"org.mariadb.jdbc.Driver","javax.jdo.option.ConnectionPassword":"{{params.HIVEPASSWORD}}","javax.jdo.option.ConnectionURL":"jdbc:mysql://devmetastore1.clmis5yhnool.ap-southeast-2.rds.amazonaws.com:3306/hive1?createDatabaseIfNotExist=true"},"Configurations":[]}, {"Classification": "capacity-scheduler","Properties": {"yarn.scheduler.capacity.resource-calculator":"org.apache.hadoop.yarn.util.resource.DominantResourceCalculator"}}, {"Classification": "yarn-site","Properties": { "yarn.resourcemanager.scheduler.class": "org.apache.hadoop.yarn.server.resourcemanager.scheduler.fair.FairScheduler"} }]' \
 --scale-down-behavior TERMINATE_AT_TASK_COMPLETION \
 --region ap-southeast-2 \
 --tags 'Name=26-7-2018-cluster01'
@@ -87,7 +87,7 @@ terminate_cluster = """
 # t0 = ExternalTaskSensor(
 #     task_id='wait_for_previous_run',
 #     trigger_rule='one_success',
-#     external_dag_id=DAG_NAME,
+#     external_dag_id=DAG_NAMEw,
 #     external_task_id='terminate_cluster',
 #     allowed_states=['success'],
 #     execution_delta=timedelta(days=1),
---
+++ dags/dag3_emr_hook.py
@@ -0,0 +1,32 @@
+from datetime import datetime, timedelta
+from airflow import DAG
+from airflow.operators.bash_operator import BashOperator
+from airflow.contrib.hooks.emr_hook import EmrHook
+from airflow.models import Variable
+
+START = datetime.combine(datetime.today() - timedelta(days=2), datetime.min.time()) + timedelta(hours=10)
+DAG_NAME = 'emr_model_building'
+# Use airflow env variables
+ENV = Variable.get("ENV", default_var="DEV")
+HIVEPASSWORD = Variable.get("HIVEPASSWORD", default_var="DEV")      # should ideally be dev.hivepassword !!!
+
+print("obtained ENV as {}".format(ENV))
+
+# initialize the DAG
+default_args = {
+    'pool': 'emr_model_building',
+    'depends_on_past': False,
+    'start_date': START,
+    'retries': 1,
+    'retry_delay': timedelta(seconds=120),
+    'email_on_failure': True,
+    'email_on_retry': True
+}
+
+dag = DAG(DAG_NAME, default_args=default_args, schedule_interval='0 1 * * *')
+
+def create_emr():
+    conn = EmrHook(emr_conn_id='emr_default')
+    response = conn.create_job_flow()
+    print (response['JobFlowId'])
+    return response
